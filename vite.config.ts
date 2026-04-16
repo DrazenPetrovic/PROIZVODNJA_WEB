95f817f144bab0e5c3f7ab7783d0b30aa17fc630
DrazenPetrovic <drazenpetrovic01@gmail.com>
2026-04-16 10:40:54 +0200

    Izmjene u konfiguraciji vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
+  base: mode === 'production' ? '/proizvodnja/' : '/',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
   server: {
     port: 5174,
   },
-});
+}));
